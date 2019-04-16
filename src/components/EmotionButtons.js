4d72a2766b237c04565d855aad641a03f2396c7a
enthye <37763349+enthye@users.noreply.github.com>
2019-04-17 09:31:19 +1200

    implemented recording button, updated emotion list

--- src/components/EmotionButtons.js
+++ src/components/EmotionButtons.js
@@ -9,8 +9,16 @@ export default class EmotionButtons extends React.Component {
                         <span role="img" aria-label="Smile">🙂</span> Happy</button>
                     <button className="button" onClick={() => this.props.setEmotionText("Sad")}>
                         <span role="img" aria-label="Sad">🙁</span> Sad</button>
+                    <button className="button" onClick={() => this.props.setEmotionText("Neutral")}>
+                        <span role="img" aria-label="Neutral">😐</span> Neutral</button>
                     <button className="button" onClick={() => this.props.setEmotionText("Angry")}>
                         <span role="img" aria-label="Angry">😠</span> Angry</button>
+                    <button className="button" onClick={() => this.props.setEmotionText("Suprise")}>
+                        <span role="img" aria-label="Suprise">😮</span> Suprise</button>
+                    <button className="button" onClick={() => this.props.setEmotionText("Disgust")}>
+                        <span role="img" aria-label="Disgust">🤢</span> Disgust</button>
+                    <button className="button" onClick={() => this.props.setEmotionText("Contempt")}>
+                        <span role="img" aria-label="Contempt">🙄</span> Contempt</button>
                     <button className="button" onClick={() => this.props.setEmotionText("Scared")}>
                         <span role="img" aria-label="Scared">😨</span> Scared</button>
                 </div>
--- src/components/SelfRecord.js
+++ src/components/SelfRecord.js
@@ -2,6 +2,38 @@ import React from 'react'
 import WebCam from 'react-webcam'
 
 export default class SelfRecord extends React.Component{
+    constructor(props){
+        super(props)
+        this.timerId=null
+        this.isRecording=false
+    }
+    
+    setRef = webcam => {
+        this.webcam = webcam
+    }
+
+    // sets up a 0.2s timer to get screenshot from cam
+    // converts from base64 to byte array
+    // fetchs face info from face azure service
+    startRecording = () => {
+        this.isRecording=true
+        this.timerId=setInterval(() => {
+            const image=this.webcam.getScreenshot()
+            const byteArrayImage=this.convertToByteArray(image)
+            this.fetchData(byteArrayImage)
+        }, 200)
+    }
+
+    convertToByteArray = (image) => {
+        const base64=require('base64-js')
+        const base64String=image.split(',')[1]
+        return base64.toByteArray(base64String)
+    }
+    
+    fetchData = (image) => {
+        
+    }
+
     render(){
         const videoConstraints={
             width:1280,
@@ -20,6 +52,7 @@ export default class SelfRecord extends React.Component{
                     screenshotFormat="image/jpeg"
                     videoConstraints={videoConstraints}
                 />
+                <button onClick={this.startRecording}>Start Recording!</button>
             </div>
         )
     }
