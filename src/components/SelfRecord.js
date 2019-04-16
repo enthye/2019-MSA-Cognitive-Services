3243d0f145ee53224842152c74294a9da534c455
enthye <37763349+enthye@users.noreply.github.com>
2019-04-17 10:57:31 +1200

    implemented emotion checker

--- src/components/SelfRecord.js
+++ src/components/SelfRecord.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import WebCam from 'react-webcam'
 
-export default class SelfRecord extends React.Component{
-    constructor(props){
+export default class SelfRecord extends React.Component {
+    constructor(props) {
         super(props)
-        this.timerId=null
-        this.isRecording=false
+        this.timerId = null
+        this.isRecording = false
     }
-    
+
     setRef = webcam => {
         this.webcam = webcam
     }
@@ -16,34 +16,72 @@ export default class SelfRecord extends React.Component{
     // converts from base64 to byte array
     // fetchs face info from face azure service
     startRecording = () => {
-        this.isRecording=true
-        this.timerId=setInterval(() => {
-            const image=this.webcam.getScreenshot()
-            const byteArrayImage=this.convertToByteArray(image)
+        this.isRecording = true
+        this.timerId = setInterval(() => {
+            const image = this.webcam.getScreenshot()
+            const byteArrayImage = this.convertToByteArray(image)
             this.fetchData(byteArrayImage)
-        }, 200)
+        }, 5000)
     }
 
     convertToByteArray = (image) => {
-        const base64=require('base64-js')
-        const base64String=image.split(',')[1]
+        const base64 = require('base64-js')
+        const base64String = image.split(',')[1]
         return base64.toByteArray(base64String)
     }
-    
-    fetchData = (image) => {
-        
+
+    fetchData = (byteArrayImage) => {
+        const apiKey = 'fa27cf3f9f024aea81d0bc88304994f6'
+        const apiEndpoint = 'https://australiaeast.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceAttributes=emotion'
+        fetch(apiEndpoint, {
+            body: byteArrayImage,
+            headers: { 'cache-control': 'no-cache', 'Ocp-Apim-Subscription-Key': apiKey, 'Content-Type': 'application/octet-stream' },
+            method: 'POST'
+        }).then(response => {
+            if (response.ok) {
+                response.json().then(data => {
+                    if (data[0] != null) {
+                        var emotion = this.emotionAnalyser(data[0].faceAttributes.emotion)
+                    }
+
+                   //var happiness = (data[0] != null ? data[0].faceAttributes.emotions.happiness : 0)
+                    //happiness = (Math.round(happiness * 100))
+                    
+/*
+                    if (this.isCapturing && happiness < 100) {
+                        this.props.emotionsArray(happiness)
+                    } else {
+                        clearInterval(this.timerId)
+                        this.isCapturing = false
+                        this.props.emotionsArray(100)
+                    }
+                    */
+                })
+            }
+        })
     }
 
-    render(){
-        const videoConstraints={
-            width:1280,
-            height:720,
-            facingMode:"user"
+    emotionAnalyser = emotionsArray => {
+        for (var emotion in emotionsArray){
+            console.log(emotion)
+            var value = Math.round(emotionsArray[emotion]) * 100
+            console.log(value)
+            if (this.isRecording && value >= 100) {
+                return emotion
+            }
+        }
+    }
+
+    render() {
+        const videoConstraints = {
+            width: 1280,
+            height: 720,
+            facingMode: "user"
         }
 
-        return(
+        return (
             <div>
-                <WebCam 
+                <WebCam
                     className="webcam"
                     audio={false}
                     width={600}
